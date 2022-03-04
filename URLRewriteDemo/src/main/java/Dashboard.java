27454d6c97b18fdf2c4cf35fdb961a093a09bcd9
PavanreddyPR <97774367+PavanreddyPR@users.noreply.github.com>
2022-03-04 12:57:49 +0530

    Phase-2

--- URLRewriteDemo/src/main/java/Dashboard.java
+++ URLRewriteDemo/src/main/java/Dashboard.java
@@ -1,50 +1,53 @@
-import java.io.*;
-import javax.servlet.*;
-import javax.servlet.annotation.*;
-import javax.servlet.http.*;
 
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
-* Servlet implementation class Dashboard
-*/
-@WebServlet("/Dashboard")
+ * Servlet implementation class Dashboard
+ */
 public class Dashboard extends HttpServlet {
-        private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
        
     /**
-* @see HttpServlet#HttpServlet()
-*/
+     * @see HttpServlet#HttpServlet()
+     */
     public Dashboard() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-        /**
-         * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-         */
-        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-                // TODO Auto-generated method stub
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
         
-            PrintWriter out = response.getWriter();
-            out.println("<html><body>");
-            
-            String userId = request.getParameter("userid");
-            if (userId == null ) {
-                out.println("No UserId was found in the URL.<br>");
-            } else {
-                out.println("UserId obtained from URL Rewriting:" + userId + "<br>");
-            }
-            out.println("</body></html>");
-
+        String userId = request.getParameter("userid");
+        if (userId == null ) {
+            out.println("No UserId was found in the URL.<br>");
+        } else {
+            out.println("UserId obtained from URL Rewriting:" + userId + "<br>");
         }
+        out.println("</body></html>");
 
-        /**
-         * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-         */
-        protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-                // TODO Auto-generated method stub
-                doGet(request, response);
-        }
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
 
 }
--- URLRewriteDemo/src/main/java/LoginServlet.java
+++ URLRewriteDemo/src/main/java/LoginServlet.java
@@ -1,42 +1,45 @@
-import java.io.*;
-import javax.servlet.*;
-import javax.servlet.annotation.*;
-import javax.servlet.http.*;
 
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
-* Servlet implementation class LoginServlet
-*/
-@WebServlet("/LoginServlet")
+ * Servlet implementation class LoginServlet
+ */
 public class LoginServlet extends HttpServlet {
-        private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
        
     /**
-* @see HttpServlet#HttpServlet()
-*/
+     * @see HttpServlet#HttpServlet()
+     */
     public LoginServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-        /**
-         * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-         */
-        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-                // TODO Auto-generated method stub
-                
-                 String userId = request.getParameter("userid");
-             response.sendRedirect("dashboard?userid=" + userId);  
-                
-        }
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+        String userId = request.getParameter("userid");
+    response.sendRedirect("dashboard?userid=" + userId);  
+       
+}
 
-        /**
-         * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-         */
-        protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-                // TODO Auto-generated method stub
-                doGet(request, response);
-        }
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
 
 }
