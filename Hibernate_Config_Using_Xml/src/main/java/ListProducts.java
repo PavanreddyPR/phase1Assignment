57647ffd9cdee52a586b6c72227d12de8827a077
PavanreddyPR <97774367+PavanreddyPR@users.noreply.github.com>
2022-03-04 12:12:30 +0530

    Phase-2

---
+++ Hibernate_Config_Using_Xml/src/main/java/ListProducts.java
@@ -0,0 +1,55 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import com.ecommerce.EProduct;
+import com.ecommerce.HibernateUtil;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.websocket.Session;
+
+
+/**
+ * Servlet implementation class ListProducts
+ */
+public class ListProducts extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	
+    public ListProducts() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+        try {
+            SessionFactory factory = HibernateUtil.getSessionFactory();
+            
+            Session session = factory.openSession();
+            // using HQL
+            List<EProduct> list = session.createQuery("from EProduct").list();
+            
+            session.close();
+            
+             PrintWriter out = response.getWriter();
+             out.println("<html><body>");
+             out.println("<b>Product Listing</b><br>");
+             for(EProduct p: list) {
+                     out.println("ID: " + String.valueOf(p.getID()) + ", Name: " + p.getName() +
+                                     ", Price: " + String.valueOf(p.getPrice()) + ", Date Added: " + p.getDateAdded().toString() + "<br>");
+             }
+             
+         out.println("</body></html>");
+         
+         
+     } catch (Exception ex) {
+             throw ex;
+     }
+     	
+	}
+	}
